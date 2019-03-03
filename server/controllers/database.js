e208725c078c1cd91feddaf9c2c6ed7989b2e71e
Julian cobos <jujocotm@gmail.com>
2019-03-02 16:24:57 -0800

    delete old seed files

---
+++ server/controllers/database.js
@@ -0,0 +1,13 @@
+const { Client } = require('pg');
+const config = require('../../config');
+
+const client = new Client(config);
+
+client.connect((err) => {
+  if (err) {
+    console.error('connection error', err.stack);
+  } else {
+    console.log('connected');
+  }
+});
+
